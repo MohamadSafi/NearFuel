89b1e5ec4e41ea136cfa89193feeaa23ca80cb0c
Mohammad Safi <the.mohammad.safi@gmail.com>
2025-06-20 11:51:20 +0300

    feat: create kodules & services for sponsor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SponsorModule } from './sponsor/sponsor.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SponsorModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
